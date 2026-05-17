d2ffa7d09cc77dfef8a665271060e2f2482b7c96
SushiNiV <sha.gmdev@gmail.com>
2026-05-17 16:02:02 +0800

    Update Initial

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST_NEW,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME_NEW,
+  connectionString: process.env.DATABASE_URL,
   // ssl: { rejectUnauthorized: false } //if ever gagamit tayo Render for hosting?
 });
 
